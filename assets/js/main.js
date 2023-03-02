cd08d0b03740fe4ff4c15ec0cc5929adf7f9e7d9
Son Thanh <coderthanhson@gmail.com>
2023-03-02 11:55:00 +0700

    update scrill reveal animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,16 @@ function scrollHeader() {
     if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader);
+/*=============== COUNT VALUE NUMBER============*/
+
 
 /*=============== SWIPER POPULAR ===============*/
 var swiperPopular = new Swiper(".popular__container", {
     spaceBetween: 32,
     grabCursor: true,
-    centeredSlides: true,
+    // centeredSlides: true,
     slidesPerView: 'auto',
-    loop: true,
+    // loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -32,7 +34,7 @@ accordionItems.forEach((item) => {
         }
     })
 });
-//handle
+//handle accordion
 const toggleItem = (item) => {
     const accordionContent = item.querySelector('.value__accordion--content');
     if (item.classList.contains('accordion-open')) {
@@ -75,6 +77,51 @@ function scrollUp() {
 window.addEventListener('scroll', scrollUp);
 
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+document.body.classList.toggle('dark-theme');
+
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun';
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+})
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+});
+sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`);
+sr.reveal(`.home__description, .footer__info`, { delay: 500 });
+sr.reveal(`.home__search`, { delay: 600 });
+sr.reveal(`.home__value`, { delay: 700 });
+sr.reveal(`.home__images`, { delay: 800, origin: 'bottom' });
+sr.reveal(`.logos__img`, { interval: 100 });
+sr.reveal(`.value__images, .contact__content`, { origin: 'left' });
+sr.reveal(`.value__content, .contact__images`, { origin: 'right' });
